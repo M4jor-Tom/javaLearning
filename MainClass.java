72011d71a6238f406bb791dc31f8fda1aa5eaf01
Tom <tom-vautray@hotmail.fr>
2021-02-10 14:23:18 +0100

    working diviner

---
+++ MainClass.java
@@ -0,0 +1,81 @@
+package javaLearning;
+import java.util.Random;
+import java.util.Scanner;
+//import java.io.IOException;
+
+public class MainClass {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		do
+		{
+			//Playing
+			playDiviner(scan);
+			System.out.println("Play again ? (1/else)");
+			
+		}while(scan.nextInt() == 1);
+		scan.close();
+	}
+
+	public static void playDiviner(Scanner scan)
+	{
+		Random rand = new Random();
+		int
+			toGuess = 0,
+			guessing = 0,
+			attempts = 0,
+			maxAttempts = 6,
+			maxValue = 100;
+		
+		//Setting up game
+		System.out.println("Select difficulty (1/2/else)");
+		switch(scan.nextInt())
+		{
+		case 1:
+			maxAttempts = 6;
+			maxValue = 100;
+			break;
+			
+		case 2:
+			maxAttempts = 12;
+			maxValue = 1000;
+			break;
+			
+		default:
+			maxAttempts = 18;
+			maxValue = 10000;
+		}
+		
+		System.out.println("2 players ? (1/else)");
+		if(scan.nextInt() == 1)
+		{
+			System.out.println("Player 1: Enter a value");
+			toGuess = (scan.nextInt() % maxValue + 1) - 1;
+			System.out.println("Number is " + toGuess);
+			//scan.next();
+			//System.out.flush();
+		}
+		else
+			toGuess = rand.nextInt(maxValue) + 1;
+			
+		
+		//Playing
+		System.out.println("Guess for a number between 1 and " +  maxValue + ":");
+		do
+		{
+			guessing = scan.nextInt();
+			++attempts;
+			System.out.println("Attempt " + attempts + "/" + maxAttempts);
+			if(attempts == maxAttempts)
+				System.out.println("Loose ! Value was " + toGuess);
+			else if(toGuess > guessing)
+				System.out.println("It's higher");
+			else if(toGuess < guessing)
+				System.out.println("It's lower");
+			else
+				System.out.println("Win !");
+		}while(toGuess != guessing && attempts < maxAttempts);
+	}
+}
